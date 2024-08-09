89ed3e0f561224d85ce1efbf09ece6b0f993ace6
zamzamnn <zamzamnoorahmed73@gmail.com>
2024-08-09 08:43:49 +0300

    react components-basics-lab

--- src/components/App.js
+++ src/components/App.js
@@ -17,12 +17,21 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About/>
+    
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
